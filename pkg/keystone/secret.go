3d5e061ca83694a8a4362b08a14f66719e8d1976
Ian Unruh <ianunruh@gmail.com>
2021-05-29 23:50:24 -0600

    keystone: Use ingress URL in admin secret

--- pkg/keystone/secret.go
+++ pkg/keystone/secret.go
@@ -13,18 +13,25 @@ func Secrets(instance *openstackv1beta1.Keystone) []*corev1.Secret {
 	labels := template.AppLabels(instance.Name, AppLabel)
 
 	return []*corev1.Secret{
-		adminSecret(instance.Name, instance.Namespace, labels),
+		adminSecret(instance.Name, instance.Namespace, labels, instance.Spec.API.Ingress),
 		fernetSecret(template.Combine(instance.Name, "credential-keys"), instance.Namespace, labels),
 		fernetSecret(template.Combine(instance.Name, "fernet-keys"), instance.Namespace, labels),
 	}
 }
 
-func adminSecret(name, namespace string, labels map[string]string) *corev1.Secret {
+func adminSecret(name, namespace string, labels map[string]string, ingress *openstackv1beta1.IngressSpec) *corev1.Secret {
 	secret := template.GenericSecret(name, namespace, labels)
+
+	var authURL string
+	if ingress == nil {
+		authURL = fmt.Sprintf("http://%s-api.%s.svc:5000/v3", name, namespace)
+	} else {
+		authURL = fmt.Sprintf("https://%s/v3", ingress.Host)
+	}
 
 	secret.StringData = map[string]string{
 		"OS_IDENTITY_API_VERSION": "3",
-		"OS_AUTH_URL":             fmt.Sprintf("http://%s-api.%s.svc:5000/v3", name, namespace),
+		"OS_AUTH_URL":             authURL,
 		"OS_REGION_NAME":          "RegionOne",
 		"OS_PROJECT_DOMAIN_NAME":  "Default",
 		"OS_USER_DOMAIN_NAME":     "Default",
