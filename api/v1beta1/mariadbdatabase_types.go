652d4718e96a41d3bc046cf581a9e0b0022b5fba
Ian Unruh <ianunruh@gmail.com>
2021-05-29 18:16:39 -0600

    mariadb: Clean up jobs on completion

--- api/v1beta1/mariadbdatabase_types.go
+++ api/v1beta1/mariadbdatabase_types.go
@@ -20,14 +20,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // MariaDBDatabaseSpec defines the desired state of MariaDBDatabase
 type MariaDBDatabaseSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
 	Cluster string `json:"cluster"`
 	Name    string `json:"name"`
 	Secret  string `json:"secret"`
@@ -35,10 +29,10 @@ type MariaDBDatabaseSpec struct {
 
 // MariaDBDatabaseStatus defines the observed state of MariaDBDatabase
 type MariaDBDatabaseStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
 	Ready bool `json:"ready"`
+
+	// +optional
+	SetupJobHash string `json:"setupJobHash"`
 }
 
 // +kubebuilder:object:root=true
