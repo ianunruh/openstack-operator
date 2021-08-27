d5e2e6f9f7290ec32fab9cddb5c4fcc8e09353db
Ian Unruh <ianunruh@gmail.com>
2021-08-27 17:08:01 -0600

    octavia: Make health manager DHCP IP permanent

--- templates/octavia/init-health-manager-port.sh
+++ templates/octavia/init-health-manager-port.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -ex
 
+HM_IFACE=o-hm0
+
 ovs-vsctl show
 
-ovs-vsctl --may-exist add-port br-int o-hm0 \
-    -- set Interface o-hm0 type=internal \
-    -- set Interface o-hm0 external-ids:iface-status=active \
-    -- set Interface o-hm0 external-ids:attached-mac=${HM_PORT_MAC} \
-    -- set Interface o-hm0 external-ids:iface-id=${HM_PORT_ID} \
-    -- set Interface o-hm0 external-ids:skip_cleanup=true
+ovs-vsctl --may-exist add-port br-int ${HM_IFACE} \
+    -- set Interface ${HM_IFACE} type=internal \
+    -- set Interface ${HM_IFACE} external-ids:iface-status=active \
+    -- set Interface ${HM_IFACE} external-ids:attached-mac=${HM_PORT_MAC} \
+    -- set Interface ${HM_IFACE} external-ids:iface-id=${HM_PORT_ID} \
+    -- set Interface ${HM_IFACE} external-ids:skip_cleanup=true
 
-ip link set dev o-hm0 address ${HM_PORT_MAC}
+ip link set dev ${HM_IFACE} address ${HM_PORT_MAC}
 
 cat > /tmp/dhclient.conf <<EOF
 request subnet-mask,broadcast-address,interface-mtu;
 do-forward-updates false;
 EOF
 
-dhclient -v o-hm0 -cf /tmp/dhclient.conf
+dhclient -v ${HM_IFACE} -cf /tmp/dhclient.conf
+
+# prevent addr from expiring
+HM_PREFIX=$(ip -4 addr show ${HM_IFACE} | awk '/inet/{print $2}')
+ip addr change ${HM_PREFIX} dev ${HM_IFACE} valid_lft forever
