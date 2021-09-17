4d54d7bf007ac519d826f05f5a3ee4ff1086a761
Ian Unruh <ianunruh@gmail.com>
2021-09-17 19:54:20 -0400

    Add basic CI gate using GKE (#32)

---
+++ gate/gke/teardown.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+CLUSTER_DOMAIN=$CLUSTER_NAME.$CLOUDSDK_COMPUTE_ZONE.test.ospk8s.com
+
+if gcloud container clusters describe $CLUSTER_NAME >/dev/null; then
+    gcloud container clusters delete $CLUSTER_NAME --quiet
+fi
+
+if gcloud dns record-sets describe "*.$CLUSTER_DOMAIN" --type=A --zone=$DNS_ZONE; then
+    gcloud dns record-sets delete "*.$CLUSTER_DOMAIN" --type=A --zone=$DNS_ZONE
+fi
+
+for disk in $(gcloud compute disks list | grep gke-$CLUSTER_NAME | awk '{print $1}'); do
+    gcloud compute disks delete $disk --quiet
+done
