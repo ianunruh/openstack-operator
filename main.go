611574471ecafbb7dbc0eeed8fadf59ac2de2efa
Ian Unruh <ianunruh@gmail.com>
2021-09-18 23:23:18 -0600

    nova: Generate scaffold for host aggregates

--- main.go
+++ main.go
@@ -370,6 +370,13 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "NovaCompute")
 		os.Exit(1)
 	}
+	if err = (&controllers.NovaHostAggregateReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "NovaHostAggregate")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
