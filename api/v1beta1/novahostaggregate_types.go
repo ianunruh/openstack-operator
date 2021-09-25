dbe1004fd1f7100dce842ca302e8965da6495409
Ian Unruh <ianunruh@gmail.com>
2021-09-25 14:46:38 -0600

    nova: Add printer columns to host aggregate

--- api/v1beta1/novahostaggregate_types.go
+++ api/v1beta1/novahostaggregate_types.go
@@ -42,6 +42,9 @@ type NovaHostAggregateStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Zone",type=string,JSONPath=`.spec.zone`
+//+kubebuilder:printcolumn:name="Ready",type=string,JSONPath=`.status.conditions[?(@.type=="Ready")].status`
+//+kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
 
 // NovaHostAggregate is the Schema for the novahostaggregates API
 type NovaHostAggregate struct {
