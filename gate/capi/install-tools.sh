a8bf2dc827bc12174592f10e0bbb0b34be618a47
Ian Unruh <me@ianunruh.com>
2024-03-01 22:17:22 -0700

    Upgrade tools, rename gdns cert issuer

--- gate/capi/install-tools.sh
+++ gate/capi/install-tools.sh
@@ -3,15 +3,15 @@ set -eux
 
 mkdir -p $HOME/.local/bin
 
-curl -sL https://dl.k8s.io/release/v1.21.7/bin/linux/amd64/kubectl -o kubectl
+curl -sL https://dl.k8s.io/release/v1.26.7/bin/linux/amd64/kubectl -o kubectl
 chmod +x kubectl
 mv kubectl $HOME/.local/bin/kubectl
 
-curl -sL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.3.0/kustomize_v4.3.0_linux_amd64.tar.gz -o kustomize.tar.gz
+curl -sL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.3.0/kustomize_v5.3.0_linux_amd64.tar.gz -o kustomize.tar.gz
 tar xfv kustomize.tar.gz
 mv kustomize $HOME/.local/bin/kustomize
 
-curl -sL https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.0.2/clusterctl-linux-amd64 -o clusterctl
+curl -sL https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.6.2/clusterctl-linux-amd64 -o clusterctl
 chmod +x clusterctl
 mv clusterctl $HOME/.local/bin/clusterctl
 
