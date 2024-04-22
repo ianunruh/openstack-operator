a8ee5fb913f901f67e1ed660e90ba7d21ecbafb9
Ian Unruh <me@ianunruh.com>
2024-04-21 22:42:53 -0600

    gate: Fix volume teardown

--- gate/capi/teardown.sh
+++ gate/capi/teardown.sh
@@ -12,30 +12,11 @@ export KUBECONFIG=$(pwd)/kubeconfig
 
 kubectl config set-context --current --namespace default
 
-log "Tearing down OpenStack control plane"
-kubectl get crd controlplanes.openstack.ospk8s.com && kubectl delete controlplane --all
-
-kubectl wait pod --for delete --all
-
-log "Cleaning up volumes"
-kubectl delete pvc --all
-
 log "Cleaning up ingress load balancer"
 kubectl -n ingress-nginx delete svc --all
 
 # Switch back to undercluster
 unset KUBECONFIG
-
-log "Waiting for all volumes to be cleaned up"
-while true; do
-    output=$(openstack volume list --long -f json | \
-        yq ".[]|select(.Properties[\"cinder.csi.openstack.org/cluster\"]==\"$CLUSTER_NAME\")")
-    if [[ "$output" -eq 0 ]]; then
-        break
-    fi
-    log "Waiting 5 more seconds for all volumes to be cleaned up"
-    sleep 5
-done
 
 log "Waiting for all load balancers to be cleaned up"
 while true; do
