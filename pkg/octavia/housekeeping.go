9f6f233e2a0daa3b303767ed875c69ad2d8b53e0
Ian Unruh <me@ianunruh.com>
2024-04-21 22:16:48 -0600

    octavia: Add more missing amphora checks, fix HM port

--- pkg/octavia/housekeeping.go
+++ pkg/octavia/housekeeping.go
@@ -20,19 +20,21 @@ func HousekeepingDeployment(instance *openstackv1beta1.Octavia, env []corev1.Env
 		template.SubPathVolumeMount("etc-octavia", "/etc/octavia/octavia.conf", "octavia.conf"),
 	}
 
+	var initContainers []corev1.Container
+
 	if instance.Spec.Amphora.Enabled {
 		volumeMounts = append(volumeMounts, amphora.VolumeMounts(instance)...)
 		volumes = append(volumes, amphora.Volumes(instance)...)
+
+		initContainers = append(initContainers, amphora.InitContainer(instance.Spec.Image, instance.Spec.Housekeeping.Resources, volumeMounts))
 	}
 
 	deploy := template.GenericDeployment(template.Component{
-		Namespace:    instance.Namespace,
-		Labels:       labels,
-		Replicas:     instance.Spec.Housekeeping.Replicas,
-		NodeSelector: instance.Spec.Housekeeping.NodeSelector,
-		InitContainers: []corev1.Container{
-			amphora.InitContainer(instance.Spec.Image, instance.Spec.Housekeeping.Resources, volumeMounts),
-		},
+		Namespace:      instance.Namespace,
+		Labels:         labels,
+		Replicas:       instance.Spec.Housekeeping.Replicas,
+		NodeSelector:   instance.Spec.Housekeeping.NodeSelector,
+		InitContainers: initContainers,
 		Containers: []corev1.Container{
 			{
 				Name:  "housekeeping",
@@ -41,11 +43,8 @@ func HousekeepingDeployment(instance *openstackv1beta1.Octavia, env []corev1.Env
 					"octavia-housekeeping",
 					"--config-file=/etc/octavia/octavia.conf",
 				},
-				Env:       env,
-				Resources: instance.Spec.Housekeeping.Resources,
-				Ports: []corev1.ContainerPort{
-					{Name: "heartbeat", ContainerPort: 5555, Protocol: corev1.ProtocolUDP},
-				},
+				Env:          env,
+				Resources:    instance.Spec.Housekeeping.Resources,
 				VolumeMounts: volumeMounts,
 			},
 		},
